9b17359ba545118f18f2bfb8a6df50195ce14758
evgni3 <evgeniy.nikulin@sitronics-kt.ru>
2024-02-04 13:18:40 +0500

    Перенаправление страниц

--- women/urls.py
+++ women/urls.py
@@ -5,9 +5,9 @@ from . import converters
 register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('', views.index),
-    path('cats/<int:cat_id>/', views.categories),
-    path('cats/<slug:cat_slug>/', views.categories_by_slug),
+    path('', views.index, name="home"),
+    path('cats/<int:cat_id>/', views.categories, name="cats_id"),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name="cats"),
     # re_path(r"^archive/(?P<year>[0-9]{4})/", views.archive)
-    path("archive/<year4:year>/", views.archive)
+    path("archive/<year4:year>/", views.archive, name="archive")
 ]
--- women/views.py
+++ women/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 def index(request):
@@ -19,6 +20,14 @@ def categories_by_slug(request, cat_slug):
 
 
 def archive(request, year):
+    if year > 2022:
+        # return redirect('/', permanent=True)
+        # return redirect(index)
+        # return redirect("home")
+        # return redirect("cats", "music")
+        uri = reverse("cats", args=("music",))
+        # return redirect(uri)
+        return HttpResponseRedirect(uri)
     if year > 2024:
         raise Http404()
     return HttpResponse(f"<h1>Архив по годам</h1> <p>Год: {year}</p>")
